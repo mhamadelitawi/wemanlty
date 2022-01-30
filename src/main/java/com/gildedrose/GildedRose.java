6a407e720644167b4b0b4bcbbd78990e4dbb8ff0
mhamadelitawi <mhamadelitawi@gmail.com>
2022-01-30 17:13:54 +0200

    separate the code by type

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,80 +10,55 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
 
+            if(item.name.equals("Sulfuras, Hand of Ragnaros"))
+                continue;
 
-
-            if(item.name.equals("Aged Brie"))
-            {
+            if (item.name.equals("Aged Brie")) {
                 increaseItemQuality(item);
-                decreaseSellIn( item);
+                decreaseSellIn(item);
                 if (item.sellIn < 0)
                     increaseItemQuality(item);
-
+                continue;
             }
-            else
-            {
 
-                if(item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
-                {
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                increaseItemQuality(item);
+                if (item.sellIn < 11)
                     increaseItemQuality(item);
-                    if (item.sellIn < 11)
-                        increaseItemQuality(item);
-                    if (item.sellIn < 6)
-                        increaseItemQuality(item);
+                if (item.sellIn < 6)
+                    increaseItemQuality(item);
 
-                    decreaseSellIn( item);
+                decreaseSellIn(item);
 
-                    if(item.sellIn < 0)
-                        item.quality = 0;
-
-                }
-                else
-                {
-                    decreaseItemQuality(item);
-
-                    decreaseSellIn( item);
-
-                    if (item.sellIn < 0)
-                        decreaseItemQuality(item);
-
-                }
-
-
+                if (item.sellIn < 0)
+                    item.quality = 0;
+                continue;
             }
 
-
+            decreaseItemQuality(item);
+            decreaseSellIn(item);
+            if (item.sellIn < 0)
+                decreaseItemQuality(item);
 
         }
     }
 
 
 
-    public Boolean checkIfSulfuras(Item item)
-    {
-        return  (item.name.equals("Sulfuras, Hand of Ragnaros"));
-    }
 
-
-    public void increaseItemQuality(Item item)
-    {
-        if(item.quality < 50)
+    public void increaseItemQuality(Item item) {
+        if (item.quality < 50)
             item.quality = item.quality + 1;
     }
 
-    public void decreaseItemQuality(Item item)
-    {
-        if(checkIfSulfuras(item)) return;
-
-            if(item.quality > 0 )
-            item.quality = item.quality -1;
+    public void decreaseItemQuality(Item item) {
+        if (item.quality > 0)
+            item.quality = item.quality - 1;
     }
 
 
-    public void decreaseSellIn(Item item)
-    {
-        if(checkIfSulfuras(item)) return;
-
-        item.sellIn = item.sellIn -1;
+    public void decreaseSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
     }
 
 
