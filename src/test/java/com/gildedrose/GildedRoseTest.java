d1ce8934f0369468ac9cd44206d508f2db03bdd9
mhamadelitawi <mhamadelitawi@gmail.com>
2022-01-30 14:20:43 +0200

    adding unit tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,97 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+
+    @Test
+    void updateNormalItem() {
+        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
+    }
+
+
+    @Test
+    void twiceFastSellByDatePassed() {
+        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void testNegativeQuality() {
+        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+
+    @Test
+    void qualityIncreaseAgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(1, app.items[0].quality);
+    }
+
+
+    @Test
+    void stableSulfuras()
+    {
+        Item[] items = new Item[] {
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80)
+
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+        assertEquals(-1, app.items[1].sellIn);
+        assertEquals(80, app.items[1].quality);
+    }
+
+
+    @Test
+    void updateBackstage()
+    {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(14, app.items[0].sellIn);
+        assertEquals(21, app.items[0].quality);
+    }
+
+
+    @Test
+    void testQualityAbove50()
+    {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+
+        assertEquals(7, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
